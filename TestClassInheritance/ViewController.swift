fc59551f618e892eeb20dd65b495ed112744220b
yuki-h29 <yuki.hirano29@gmail.com>
2023-06-15 04:00:51 +0900

    Initial Commit

---
+++ TestClassInheritance/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestClassInheritance
+//
+//  Created by 平野裕貴 on 2023/06/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
